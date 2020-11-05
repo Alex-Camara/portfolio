051fc40299a1759a62917b6a41081aad811f041e
Alejandro <arcam_2112@hotmail.com>
2020-11-05 14:24:05 -0600

    added currently used technologies and bicket list

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -1,14 +1,14 @@
 <template>
-    <div ref="compAboutMe" class="flex flex-col h-full lg:h-screen bg-primary">
+    <div ref="compAboutMe" class="flex flex-col h-full">
 
-        <div class="flex flex-col p-4 lg:flex-row  mx-5 mt-5 lg:mx-40 lg:mt-20 my-auto">
+        <div class="flex flex-col p-4 lg:flex-row  mx-5 mt-5 lg:mx-24 lg:mt-20 my-auto">
 
-                <div class="flex flex-col lg:w-7/12">
+                <div class="flex flex-col lg:w-9/12 lg:pr-10">
                     <section-title :title="title" data-aos="fade-down" data-aos-duration="3000" data-aos-once="true"/>
                     <div class="text-base lg:text-lg text-dark-white" data-aos="fade-up" data-aos-duration="2000" data-aos-once="true">
-                        <p class="mt-4 mb-8">{{ paragraph1 }}</p>
-                        <p class="mb-8">{{ paragraph2 }}</p>
-                        <p class="mb-8">{{ paragraph3 }}</p>
+                        <p class="mt-4 mb-4">{{ paragraph1 }}</p>
+                        <p class="mb-4">{{ paragraph2 }}</p>
+                        <p class="mb-4">{{ paragraph3 }}</p>
                         <p>{{ paragraph4 }}</p>
                     </div>
                 </div>
@@ -16,6 +16,29 @@
                 <div class="flex h-full items-center m-5 lg:w-4/12 lg:ml-5">
                     <img src="../assets/me.jpg" class="rounded-md max-w-x1 lg:accent-filter hover:no-filter ease-in-out duration-300">
                 </div>
+        </div>
+
+        <div class="flex flex-col p-4 mx-5 mt-2 lg:mx-24">
+
+            <div class="flex flex-col lg:mx-auto">
+                <p class="text-dark-white lg:text-lg">Some technologies I've recently used:</p>
+                <div class="flex flex-col flex-wrap h-32 mt-4 ">
+                    <div class="flex items-center pr-3 py-1 text-dark-white text-sm" v-for="interest in interests" :key="interest">
+                        <p class="bullet-text order-2">{{ interest }}</p>
+                        <div class="bullet order-1"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mt-8 flex flex-col lg:mx-auto" >
+                <p class="text-dark-white lg:text-lg">Bucket list</p>
+                <div class="flex flex-col flex-wrap mt-4">
+                    <div class="flex items-center pr-3 py-1 text-dark-white text-sm" v-for="bucketItem in bucketList" :key="bucketItem">
+                        <p class="bullet-text order-2">{{ bucketItem }}</p>
+                        <div class="bullet order-1"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -33,8 +56,12 @@ export default class AboutMe extends Vue {
     private paragraph1 = 'Hi! I\'m Alex, a mexican software engineer.';
     private paragraph2 = 'I\'m advocated to create good quality software solutions. Eager to work as part of an awesome team to bring efficient solutions to complex problems.';
     private paragraph3 = 'I completed my bachelor of Software Engineering at Universidad Veracruzana, during the 2020 summer.';
-    private paragraph4 = 'My hobbies include playing with my cat, watching my cat play, watching my cat sleep, taking pictures of my cat and so on…!';
-    
+    private paragraph4 = 'My hobbies include gardening, cooking, learning new things and playing with my cat. Lately I enjoy working on side projects '
+                        + 'that help me acquire new skills as well as to learn how thing work. I am especially hooked with the use of Ruby on Rails as '
+                        + 'an API REST backend service and Vue js to consume the API.';
+    private interests = ["Vue", "Ruby on Rails", "Ruby", "JavaScript", "Flutter", "Web development", "Mobile"];
+    private bucketList = ["Flutter"];
+
     public scrollToTop() {
         var rootElement = this.$refs.compAboutMe as HTMLDivElement;
         var offsets = rootElement.getBoundingClientRect();
