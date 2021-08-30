a67dc9f46928478ee14a09f72a4dd3914695c0c1
Alex Cámara <arcamsoft@gmail.com>
2021-08-30 15:08:28 -0500

    Update cv: corrected style

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -37,7 +37,7 @@
             </div> 
         </div>
         <div class="hidden md:flex md:visible items-center" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="900">
-            <a href="./cv-alex-camara-software-engineer.pdf" class="regular-button mr-10" target="_blank">
+            <a href="./cv_alejandro_camara_software_engineer.pdf" class="regular-button mr-10" target="_blank">
                 {{ resumeButtonText }}
             </a>
         </div>
